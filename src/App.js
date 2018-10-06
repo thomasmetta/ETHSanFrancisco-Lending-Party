7175288e7cfbfb97cab903cc6340507485b1b529
Thomas Lui <tlfit28@gmail.com>
2018-10-06 12:55:34 -0400

    Add bloom QR component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import {RequestQRCode, RequestData} from '@bloomprotocol/share-kit'
 
 const MyComponent: React.SFC = props => {
-  const requestData: RequestData = {
+  const requestData = {
+  action: "request_attestation_data",
   token: '0x8f31e48a585fd12ba58e70e03292cac712cbae39bc7eb980ec189aa88e24d043',
-  url: 'https://bloom.co/api/receiveData',
+  url: 'https://78651dda.ngrok.io/',
   org_logo_url: 'https://bloom.co/images/notif/bloom-logo.png',
   org_name: 'Bloom',
   org_usage_policy_url: 'https://bloom.co/legal/terms',
   org_privacy_policy_url: 'https://bloom.co/legal/privacy',
-  types: ['full-name', 'phone', 'email'],
+  types: ['phone', 'email'],
 }
   return <RequestQRCode requestData={requestData} size={200} />
 }
--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,21 @@ import LockEth from './LockEth.js';
 import DrawDai from './DrawDai.js';
 import WipeDebt from './WipeDebt.js';
 import ShutCdp from './ShutCdp.js';
+import {RequestQRCode, RequestData} from '@bloomprotocol/share-kit'
+
+const BloomQRComponent: React.SFC = props => {
+  const requestData = {
+  action: "request_attestation_data",
+  token: '0x8f31e48a585fd12ba58e70e03292cac712cbae39bc7eb980ec189aa88e24d043',
+  url: 'https://78651dda.ngrok.io/',
+  org_logo_url: 'https://bloom.co/images/notif/bloom-logo.png',
+  org_name: 'Bloom',
+  org_usage_policy_url: 'https://bloom.co/legal/terms',
+  org_privacy_policy_url: 'https://bloom.co/legal/privacy',
+  types: ['phone', 'email'],
+}
+  return <RequestQRCode requestData={requestData} size={200} />
+}
 
 class App extends Component {
 
@@ -19,6 +34,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">MakerJS React Redux Example</h1>
         </header>
+          <br/>
+          <BloomQRComponent/>
           <br/>
           <StartButton/>
           <br /><p className="App-intro"><strong>This application pulls in maker.js to open a cdp, lock eth, draw dai, repay the dai, and then shut the cdp.  Each transaction is sent after the other is mined.  Progress is reported below.  Click Start to begin.</strong></p><br />
