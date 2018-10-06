8365f8d6a1056507de276c0d15bee16fe2e36d54
Thomas Lui <tlfit28@gmail.com>
2018-10-06 02:43:27 -0400

    Install bloom and generate QR code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {RequestQRCode, RequestData} from '@bloomprotocol/share-kit'
+
+const MyComponent: React.SFC = props => {
+  const requestData: RequestData = {
+  token: '0x8f31e48a585fd12ba58e70e03292cac712cbae39bc7eb980ec189aa88e24d043',
+  url: 'https://bloom.co/api/receiveData',
+  org_logo_url: 'https://bloom.co/images/notif/bloom-logo.png',
+  org_name: 'Bloom',
+  org_usage_policy_url: 'https://bloom.co/legal/terms',
+  org_privacy_policy_url: 'https://bloom.co/legal/privacy',
+  types: ['full-name', 'phone', 'email'],
+}
+  return <RequestQRCode requestData={requestData} size={200} />
+}
 
 class App extends Component {
   render() {
@@ -17,7 +31,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <MyComponent/>
           </a>
         </header>
       </div>
