fccf036f4e1d05cd43c9266eb20294588395922a
Brandon In <brandonin@me.com>
2018-10-07 07:07:46 -0700

    added wyre

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,17 @@ import io from 'socket.io-client';
 import scrollToComponent from 'react-scroll-to-component';
 import PieChart from 'react-minimal-pie-chart';
 
+const WyreClient = require('@wyre/api').WyreClient
+// import {WyreClient} from '@wyre/api'
+var session = process.env.WYRE_SUBSCRIPTION;
+
+let wyre = new WyreClient({
+  format: "json_numberstring",
+  apiKey: process.env.WYRE_API_KEY,
+  secretKey: process.env.WYRE_SECRET_KEY,
+  baseUrl: "https://api.testwyre.com/"
+})
+
 const socket = io("https://daab90ce.ngrok.io");
 
 const BloomQRComponent: React.SFC = props => {
@@ -111,6 +122,34 @@ class App extends Component {
       maxDebtCombined: Math.round(maxDebtCombined*100)/100,
     });
 
+    // Do transfer of Dai to Wyre for USD to bank
+    // transferDai("0xf6aea8fd7b296aff67fc1526abd80cb38b84b523")
+    //   .then(console.log)
+    //   .catch(console.log);
+
+    function transferDai(ethAddress) {
+      return wyre.post("/transfers", {
+        dest: {
+          paymentMethodType: "INTERNATIONAL_TRANSFER",
+          paymentType: "LOCAL_BANK_WIRE",
+          currency: "USD",
+          country: "US",
+          beneficiaryType: "INDIVIDUAL",
+          firstNameOnAccount: "Brandon",
+          lastNameOnAccount: "In",
+          beneficiaryPhoneNumber: "1234567890",
+          accountNumber: "111111111111111",
+          routingNumber: "111111111",
+          accountType: "CHECKING",
+          chargeablePM: true,
+          bankName: "Bank of America"
+        },
+        destCurrency: "USD",
+        sourceCurrency: "DAI",
+        destAmount: amount,
+        autoConfirm: true
+      })
+    }
   }
 
   async componentDidMount() {
